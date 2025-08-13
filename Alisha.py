bd281effbca13902d7d6b52c199f8096aa2be4a7
Alisha Gurung <gurungalisha006@gmail.com>
2025-08-13 17:53:08 +0545

    deleted file

--- Alisha.py
+++
@@ -1,7 +0,0 @@
-name="Alisha",
-age=20,
-email="gurungalisha2@example.com",
-address="baglung",
-phone_number='25609872'
-
-print(f"Name: {name}\n Age:{age}\n Email:{email}\n Address: {address}\n Phone_number:{phone_number}")
