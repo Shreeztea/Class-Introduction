bb1f1a9abbcdfd82f0702f0f36f1415fa8102f11
Alisha Gurung <gurungalisha006@gmail.com>
2025-08-13 17:14:59 +0545

    Personal information

---
+++ Alisha.py
@@ -0,0 +1,7 @@
+name="Alisha",
+age=20,
+email="gurungalisha2@example.com",
+address="baglung",
+phone_number='25609872'
+
+print(f"Name: {name}\n Age:{age}\n Email:{email}\n Address: {address}\n Phone_number:{phone_number}")
